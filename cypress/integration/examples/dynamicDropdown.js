bc509f3133dfb57b3157191c8c2c1b22f65ee4ff
abakarovmaks <abakarovmaks@gmail.com>
2023-10-01 17:03:27 +0300

    Add tests for dynamic dropdown

---
+++ cypress/integration/examples/dynamicDropdown.js
@@ -0,0 +1,21 @@
+/// <reference types='Cypress'/>
+
+describe('Dynamic dropdown', () => {
+  it('Dynamic dropdown', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
+    // Select and verify one of the dynamic dropdowns select
+    cy.get('#autocomplete').type('ind');
+
+    // Create cycle, find text India in dynamic dropdown, verify it and click
+    cy.get('.ui-menu-item div').each(($el, index, list) => {
+      const textVeg = $el.text();
+      if (textVeg === 'India') {
+        cy.wrap($el).click();
+      }
+    });
+
+    // Verify that value is India
+    cy.get('#autocomplete').should('have.value', 'India');
+  });
+});
