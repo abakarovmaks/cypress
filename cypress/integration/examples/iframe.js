9bfa775798edb12f92391548aeb0d03a2eb700df
abakarovmaks <abakarovmaks@gmail.com>
2023-10-03 12:17:59 +0300

    Add test for iframe

---
+++ cypress/integration/examples/iframe.js
@@ -0,0 +1,19 @@
+/// <reference types='Cypress'/>
+/// <reference types='cypress-iframe'/>
+import 'cypress-iframe';
+
+describe('Handling Tab', () => {
+  it('Handling Tab', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
+    // Open the iframe
+    cy.frameLoaded('#courses-iframe');
+
+    // Switch to iframe mode
+    cy.iframe().find('a[href*="mentorship"]').eq(0).click();
+
+    // Assert the length of the h1 on iframe page
+    cy.wait(1000);
+    cy.iframe().find('h1[class*="pricing-title"]').should('have.length', 2);
+  });
+});
