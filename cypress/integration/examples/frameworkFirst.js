4dde145f976f3e8c83f1a80637b72f1365d490f5
abakarovmaks <abakarovmaks@gmail.com>
2023-10-03 15:14:39 +0300

    Add test with using custom command

--- cypress/integration/examples/frameworkFirst.js
+++ cypress/integration/examples/frameworkFirst.js
@@ -12,5 +12,26 @@ describe('Framework 1st', () => {
   it('Framework 1st', () => {
     cy.get(':nth-child(1) > .form-control').type(globalThis.data.name);
     cy.get('select').select(globalThis.data.gender);
+
+    // Verify that name is the same
+    cy.get(':nth-child(4) > .ng-untouched').should(
+      'have.value',
+      globalThis.data.name
+    );
+
+    // Verify that attribute minlength is equal 2
+    cy.get(':nth-child(1) > .form-control').should(
+      'have.attr',
+      'minlength',
+      '2'
+    );
+
+    // Verify that radio button is disabled
+    cy.get('#inlineRadio3').should('be.disabled');
+
+    cy.get(':nth-child(2) > .nav-link').click();
+    // Use custom command
+    cy.selectProduct('Blackberry');
+    cy.selectProduct('iphone X');
   });
 });
