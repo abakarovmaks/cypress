bb12823bdae713eb7cd519357a2a56a0b11e7109
abakarovmaks <abakarovmaks@gmail.com>
2023-10-06 17:27:20 +0300

    Add Api test

---
+++ cypress/integration/examples/apiTest.js
@@ -0,0 +1,23 @@
+/// <reference types='Cypress'/>
+
+describe('Api test', () => {
+  before(() => {
+    // cy.visit(Cypress.env('url') + '/angularAppdemo/');
+  });
+
+  it('Security test', () => {
+    cy.request({
+      method: 'POST',
+      url: 'http://216.10.245.166/Library/Addbook.php',
+      body: {
+        name: 'Learn Appium Automation with Java',
+        isbn: 'Maks_Abakarov2',
+        aisle: '997',
+        author: 'Abakarov Maksym2',
+      },
+    }).then((res) => {
+      expect(res.body).to.have.property('Msg', 'successfully added');
+      expect(res.status).to.eq(200);
+    });
+  });
+});
